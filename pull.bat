1acaed6809e9232ebadc11f16038d3db8a2afeb9
Hottinger Thomas <thomas.hottinger@luks.ch>
2018-06-08 16:34:39 +0200

    remove ~files

--- pull.bat
+++ pull.bat
@@ -3,6 +3,7 @@ call setEnv.bat
 set http_proxy=http://hottinth:DHimH.Mwnm.@proxy.gsdnet.ch:8080
 set https_proxy=https://hottinth:DHimH.Mwnm.@proxy.gsdnet.ch:8080
 set GIT_SSL_NO_VERIFY=true
+rm -rf *~
 @echo on
 git pull 
 pause
