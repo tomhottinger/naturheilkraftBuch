e35c44b92e80444ea9b23eb1dcca80d81c50bf57
Hottinger Thomas <thomas.hottinger@luks.ch>
2018-06-08 16:40:25 +0200

    added Caret Executable

--- setEnv.bat
+++ setEnv.bat
@@ -3,7 +3,7 @@
 for %%x in (%0) do set ThisBase=%%~dpsx
 for %%x in (%ThisBase%) do set ThisBase=%%~dpsx
 
-set PATH=%ThisBase%\bin\pandoc\;%ThisBase%\bin\wbin\;%ThisBase%\bin\PortableGit\cmd\;%ThisBase%\bin\kindlegen\;%ThisBase%\bin\SeaMonkeyPortable\;%PATH%
+set PATH=%ThisBase%\bin\pandoc\;%ThisBase%\bin\wbin\;%ThisBase%\bin\PortableGit\cmd\;%ThisBase%\bin\kindlegen\;%ThisBase%\bin\SeaMonkeyPortable\;%ThisBase%\bin\Caret\;%PATH%
 
 set http_proxy=http://hottinth:DHimH.Mwnm.@proxy.gsdnet.ch:8080
 set https_proxy=https://hottinth:DHimH.Mwnm.@proxy.gsdnet.ch:8080
